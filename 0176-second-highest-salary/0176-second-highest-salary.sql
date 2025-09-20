a8dd85e3e0df188a87edd97a72bff31b1666ee16
KimDohee <kheedogg@naver.com>
2025-09-20 17:33:24 +0900

    Time: 240 ms (96.32%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,17 @@
 # Write your MySQL query statement below
-SELECT
-    (
-        SELECT DISTINCT salary 
-        FROM Employee 
-        GROUP BY salary 
-        ORDER BY salary DESC
-        LIMIT 1
-        OFFSET 1
-    ) AS SecondHighestSalary
+
+## 1)
+-- SELECT
+--     (
+--         SELECT DISTINCT salary 
+--         FROM Employee 
+--         ORDER BY salary DESC
+--         LIMIT 1
+--         OFFSET 1
+--     ) AS SecondHighestSalary
+
+
+## 2)
+select max(salary) as SecondHighestSalary 
+from employee 
+where salary < (select max(salary) from employee)
